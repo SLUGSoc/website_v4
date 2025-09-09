fbb5bbac96380fc8badd53699924a4ecf8ea5778
OGD <114223604+OGD311@users.noreply.github.com>
2025-09-09 14:28:13 +0100

    Added TS interfaces for esports, committee members, events.

---
+++ src/constants/interfaces.ts
@@ -0,0 +1,19 @@
+export interface COMMITTEE {
+    name: string,
+    role: string,
+    favourite_game: string,
+    bio: string,
+
+    importance: number
+}
+
+export interface ESPORT {
+    discord: string,
+    colour: string
+}
+
+export interface EVENT {
+    name: string,
+    startDate: Date,
+    endDate: Date,
+}
