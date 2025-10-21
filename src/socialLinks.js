a47a3e66e92d80340672bd5388450a09e190fb26
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-10-21 09:24:47 +0100

    Added linktree link.

--- src/socialLinks.js
+++ src/socialLinks.js
@@ -1,5 +1,6 @@
 export default {
     "discord" : "https://discord.gg/VvUCXCT",
     "instagram": "https://www.instagram.com/slugsoc/",
-    "twitter": "https://x.com/SLUGSoc"
+    "twitter": "https://x.com/SLUGSoc",
+    "linktree": "https://linktr.ee/slugsoc"
 }
